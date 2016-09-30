864cb262938acde5843e590a7fa35f5ed08c086f
Ryu Shinmura <rysestock@gmail.com>
2016-09-30 13:21:21 +0900

    静的なサイトマップをローカルで作成するようにした

--- _plugins/hook-add-last-modified-date.rb
+++ _plugins/hook-add-last-modified-date.rb
@@ -1,7 +1,12 @@
+require 'fileutils'
 Jekyll::Hooks.register :site, :pre_render do |site, payload|
 #Just before rendering the whole site
     for page in payload['site']['pages']
         modification_time = File.mtime( page.path )
         page.data['last-modified-date'] = modification_time
     end
+end
+Jekyll::Hooks.register :site, :post_write do |site, payload|
+#Just before rendering the whole site
+    FileUtils.cp('K:/Programming/workspace/ryu-s.github.io/_site/presitemap.xml','K:/Programming/workspace/ryu-s.github.io/sitemap.xml')
 end
